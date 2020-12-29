789fea216a22f3f190210de8dc399b311a1accf8
jogog <jogog007@mail.ru>
2020-12-29 22:46:37 +0300

    init

---
+++ app/src/main/java/com/jogog/chatapp/ResetPasswordActivity.java
@@ -0,0 +1,55 @@
+package com.jogog.chatapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    EditText send_email;
+    Button btn_reset;
+
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Reset Password");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        send_email = findViewById(R.id.send_email);
+        btn_reset = findViewById(R.id.btn_reset);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        btn_reset.setOnClickListener(view -> {
+            String email = send_email.getText().toString();
+
+            if (email.equals("")) {
+                Toast.makeText(ResetPasswordActivity.this, "All fileds are required!", Toast.LENGTH_SHORT).show();
+            } else {
+                firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(ResetPasswordActivity.this, "Please check you Email", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(ResetPasswordActivity.this, LoginActivity.class));
+                    } else {
+                        String error = task.getException().getMessage();
+                        Toast.makeText(ResetPasswordActivity.this, error, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+    }
+}
